1b13a9d51e6fc13568d45dfdb6a650a45f64ff11
li tong <lililililitongg@gmail.com>
2021-05-27 12:57:55 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/p06_ps/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p06_ps/MainActivity.java
@@ -1,14 +1,49 @@
 package com.myapplicationdev.android.p06_ps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    FragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewPager);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+        al.add(new Frag3());
+
+//        adapter = new FragmentPagerAdapter(fm, al);
+//
+//        vPager.setAdapter(adapter);
+
+        btnClose = findViewById(R.id.button);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
